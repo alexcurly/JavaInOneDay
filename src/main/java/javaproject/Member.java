184c177ee06c0ded6f8978ee42154df8cebe3396
alexcurly <69361733+alexcurly@users.noreply.github.com>
2023-09-14 00:08:03 +0300

    Initial commit

---
+++ src/main/java/javaproject/Member.java
@@ -0,0 +1,57 @@
+package javaproject;
+
+public class Member {
+    private char memberType;
+    private int memberID;
+    private String name;
+    private double fees;
+
+    Member(char pMemberType, int pMemberID, String pName, double pFees) {
+        this.memberType = pMemberType;
+        this.memberID = pMemberID;
+        this.name = pName;
+        this.fees = pFees;
+    }
+
+    public void setMemberType(char pMemberType) {
+        memberType = pMemberType;
+    }
+
+    public char getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberID(int pMemberID) {
+        memberID = pMemberID;
+    }
+
+    public int memberID() {
+        return memberID;
+    }
+
+    public void setName(String pName) {
+        name = pName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setFees(double pFees) {
+        fees = pFees;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    @Override
+    public String toString()
+    {
+        return memberType + ", "
+                + memberID + ", "
+                + name + ", "
+                + fees;
+    }
+
+}
